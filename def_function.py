3263b5222802dcb9bf14a542ec2594a997768274
Gaurav Khandelwal <gauravkhandelwal1005@gmail.com>
2026-09-19 09:16:14 +0530

    Refactor function and add currency converter
    Fix variable name and add functionality for number validation and currency conversion.

---
+++ def_function.py
@@ -0,0 +1,29 @@
+countries = ["India","China","Australia","US","Japan"]
+
+def num_val():
+    for i in countries:
+      if(len(i) % 2 == 0):
+        print(f"{i} , = Even"}
+      else:
+        print(f"{i} , = ODD"}
+
+num_val()
+
+def converter(usd_converter):
+  inr_converter = usd * 97
+  print(usd_converter , "USD = " , inr_converter, "INR")
+
+converter(5)
+
+def num_val(n):
+  for i in range(1 , n+1):
+    fact *= i
+    print(fact)
+
+num_val(5)
+ 
+
+
+            
+        
+      
