59355d1d7a7869c1bd794ed85967f79848d3aa1b
Gaurav Khandelwal <gauravkhandelwal1005@gmail.com>
2026-09-20 08:50:44 +0530

    Fix function name and call for country list

---
+++ function.py
@@ -0,0 +1,43 @@
+def cal_sum():
+  sum = a + b
+  print(sum)
+  return(sum)
+
+cal_sum(5, 10)
+
+def print_hello():
+  print("hello world")
+
+print_hello()
+
+def avg(a, b, c):
+  sum = a + b + c
+  avg = a + b + c/3
+  print(sum)
+  return(sum)
+
+avg (1, 15, 50)
+
+Countries = ["India", "Australia","USA","Japan","China"]
+Movies = ["Mirzapur","DJ","3 Idiots"]
+
+def name_char(Countires):
+  print(len(Countries)
+  print(len(Movies)
+
+name_char(Countries)
+name_char(Movies)
+
+Countries = ["India","Australia","USA","Japan","China"]
+
+def name_len(list):
+  for item in list():
+       print(item , end ="")
+
+name_list(Countries)
+print()
+  
+  
+
+
+        
