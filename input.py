40ebd00b6fef50ef870e11892b503ad309fd4faa
Gaurav Khandelwal <gauravkhandelwal1005@gmail.com>
2026-09-24 21:03:07 +0530

    Add voting eligibility and greatest number logic
    Implement age voting eligibility check and number comparisons.

---
+++ input.py
@@ -0,0 +1,37 @@
+age = 14
+
+if(age >= 18):
+    print("can vote")
+elif(age == 18):
+    print("cannt vote")
+else:
+    print("anybody not vote")
+
+print("bjp can not vote")
+
+num = 45
+
+a = num % 2
+
+if(num % 2 == 0):
+    print("even number")
+else:
+    print("odd number")
+
+a = int(input("first number: "))
+b = int(input("second number: "))
+c = int(input("third number: "))
+d = int(input("forth number: "))
+
+if(a >= b and a>=c):
+    print("first number is greatest",a)
+elif(b >= c):
+    print("second number is greatest",b)
+elif(b >= c and c >= d):
+    print("third number is greatest",c)
+else:
+    print("forth number is greatest",d)
+
+
+
+        
