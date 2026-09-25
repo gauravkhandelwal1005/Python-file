12ff0c37130abde5bb9d766039bf79da681c0448
Gaurav Khandelwal <gauravkhandelwal1005@gmail.com>
2026-09-25 19:39:22 +0530

    Implement arithmetic and string functions in string.py
    Add basic arithmetic operations and string manipulations

---
+++ string.py
@@ -0,0 +1,27 @@
+a = 50
+b = 30 
+
+sum = a + b
+print(sum)
+
+a = int(input("Enter first number: "))
+b = int(input("ENter second number: "))
+
+print("sum =", a + b)
+
+a = float(input("Enter square side: "))
+
+print("sum =", a * a)
+
+str1 = "Gaurav"
+str2 = "Khandelwal"
+
+print(str1  + str2)
+
+str = "Gaurav khandelwal"
+print(str[7:len(str)])
+print(str[:7])
+
+str = "i am studying from python from youtude"
+str = str.capitalize()
+str = str.count("o")
