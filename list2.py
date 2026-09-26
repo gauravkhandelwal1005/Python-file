e20ed9e996559a74f2aeaad1550ed1cf8d22a5c8
Gaurav Khandelwal <gauravkhandelwal1005@gmail.com>
2026-09-26 11:08:55 +0530

    Add movie input and palindrome check functionality

---
+++ list2.py
@@ -0,0 +1,20 @@
+movies = []
+
+mov1 = (input("enter the first movie: "))
+movies.append(mov1)
+mov2 = (input("enter the second movie: "))
+movies.append(mov2)
+mov3 = (input("enter the third movie: "))
+movies.append(mov3)
+
+print("movies")
+
+list = [1,2,1]
+
+list_count = list.copy()
+list.reverse()
+
+if(list_count == list):
+    print("palidrome")
+else:
+    print("not palidrome")
